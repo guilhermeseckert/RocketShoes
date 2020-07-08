608a4bb70a02df656d7f75e1ab677e93481b45dc
Guilherme Sonnemann Eckert <guilhermeeckert@Guilhermes-MacBook-Pro.local>
2020-07-08 00:54:45 -0700

    Updates

---
+++ precache-manifest.31f3df2fa9e46fe8daee1aa6717af072.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3568c71d64f0e2dff2487cdbf50a8235",
+    "url": "/RocketShoes/index.html"
+  },
+  {
+    "revision": "b40ae0647767fa8a327a",
+    "url": "/RocketShoes/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "b40ae0647767fa8a327a",
+    "url": "/RocketShoes/static/js/2.d75b5f29.chunk.js"
+  },
+  {
+    "revision": "de1cc2dd24048f0e036186d71803b30a",
+    "url": "/RocketShoes/static/js/2.d75b5f29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8575b5387d684da7ad3c",
+    "url": "/RocketShoes/static/js/main.f8939b58.chunk.js"
+  },
+  {
+    "revision": "764705f5ce6aef19de8e",
+    "url": "/RocketShoes/static/js/runtime-main.24260f77.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/RocketShoes/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/RocketShoes/static/media/logo.879557c5.svg"
+  }
+]);
